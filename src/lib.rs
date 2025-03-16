72482c26cf15ae68f26be9c3cee69f8cc5dec36f
mohamed-abd-elkahlk <01147018091.m@gmail.com>
2025-03-16 20:06:42 +0200

    add UserUpdate

--- src/lib.rs
+++ src/lib.rs
@@ -35,11 +35,19 @@ pub struct LoginUserRequest {
     pub email: String,
     pub password: String,
 }
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+pub struct UpdateUser<U = ()> {
+    pub username: Option<String>,
+    pub email: Option<String>,
+    pub password: Option<String>,
+    pub role: Option<Role>,
+    pub data: Option<U>,
+}
 #[async_trait]
 pub trait UserRepository<U>: Send + Sync {
     async fn find_user_by_id(&self, id: &str) -> Result<User<U>, AuthError>;
     async fn find_user_by_email(&self, email: &str) -> Result<User<U>, AuthError>;
-    async fn create_user(&self, user: User<U>) -> Result<(), AuthError>;
-    async fn update_user(&self, user: User<U>) -> Result<(), AuthError>;
+    async fn create_user(&self, user: RegisterUserRequest) -> Result<User<U>, AuthError>;
+    async fn update_user(&self, user: UpdateUser<U>) -> Result<User<U>, AuthError>;
     async fn delete_user(&self, id: &str) -> Result<(), AuthError>;
 }
